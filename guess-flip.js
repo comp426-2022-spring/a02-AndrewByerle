5947b6403b182907c0e5d11ab7a6cb5331b204ca
Andrew Byerle <andran@live.unc.edu>
2022-02-05 13:47:51 -0500

    completed guess file

---
+++ guess-flip.js
@@ -0,0 +1,9 @@
+import minimist from 'minimist';
+import { coinFlips, countFlips, flipACoin } from './modules/coin.mjs';
+
+const argv = minimist(process.argv.slice(2));
+// console.log(argv)
+if (argv.call == null || (argv.call != "heads") && (argv.call != "tails")){
+    throw new Error('no input. Usage: node guess-flip --call=[heads|tails]');
+}
+console.log(flipACoin(argv.call));
